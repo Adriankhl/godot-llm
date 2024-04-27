ad49071456b3fa45aea5de296dddd90c39d8dd7d
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-27 20:50:05 +0800

    refactor: rename register functions

--- src/register_types.h
+++ src/register_types.h
@@ -3,9 +3,9 @@
 
 #include <godot_cpp/core/class_db.hpp>
 
-using namespace godot;
-
-void initialize_example_module(ModuleInitializationLevel p_level);
-void uninitialize_example_module(ModuleInitializationLevel p_level);
+namespace godot {
+    void initialize_llm_module(ModuleInitializationLevel p_level);
+    void uninitialize_llm_module(ModuleInitializationLevel p_level);
+}
 
 #endif // GDEXAMPLE_REGISTER_TYPES_H
