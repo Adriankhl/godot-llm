0f6951a4af69207d4d2c4128c111d0571a31bd70
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-27 17:46:52 +0800

    Allow setting model paths

--- src/gdllama.cpp
+++ src/gdllama.cpp
@@ -1,14 +1,28 @@
 #include "gdllama.h"
+#include "llama.h"
+#include <string>
 
 namespace godot {
     void GDLlama::_bind_methods() {
-
+    	ClassDB::bind_method(D_METHOD("get_model_path"), &GDLlama::get_model_path);
+    	ClassDB::bind_method(D_METHOD("set_model_path", "p_model_path"), &GDLlama::set_model_path);
+    	ClassDB::add_property("GDLlama", PropertyInfo(Variant::STRING, "model_path"), "set_amodel_path", "get_model_path");
     }
 
     GDLlama::GDLlama() {
+        llama_backend_init();
+        //llama_numa_init(g_params->numa);
     }
 
     GDLlama::~GDLlama() {
 
+    }
+
+    std::string GDLlama::get_model_path() const {
+        return params->model;
+    }
+
+    void GDLlama::set_model_path(const std::string p_model_path) {
+        params->model = p_model_path;
     }
 }
--- src/gdllama.h
+++ src/gdllama.h
@@ -4,18 +4,19 @@
 #include <godot_cpp/classes/node.hpp>
 #include <common/common.h>
 #include <llama.h>
+#include <string>
 
 namespace godot {
     class GDLlama : public Node {
         GDCLASS(GDLlama, Node)
 
         private:
-            llama_context           ** g_ctx;
-            llama_model             ** g_model;
-            gpt_params               * g_params;
-            std::vector<llama_token> * g_input_tokens;
-            std::ostringstream       * g_output_ss;
-            std::vector<llama_token> * g_output_tokens;
+            llama_context            * ctx;
+            llama_model              * model;
+            gpt_params               * params;
+            std::vector<llama_token> * input_tokens;
+            std::ostringstream       * output_ss;
+            std::vector<llama_token> * output_tokens;
             bool is_interacting = false;
 
 
@@ -25,6 +26,8 @@ namespace godot {
         public:
             GDLlama();
             ~GDLlama();
+            std::string get_model_path() const;
+            void set_model_path(const std::string model_path);
     };
 }
 
