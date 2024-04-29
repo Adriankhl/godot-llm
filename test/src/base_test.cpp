225b089e8bdf4d85ae41e6e67022a4564c1eee19
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-29 09:00:39 +0800

    test: update model path

---
+++ test/src/base_test.cpp
@@ -0,0 +1,36 @@
+#include "../../src/llama_runner.h"
+#include "common.h"
+#include <iostream>
+#include <ostream>
+#include <string>
+
+int main(int argc, char ** argv) {
+    //Calling godot::String produces user mode data execution error exception
+    //Probably need the full engine to call godot-related classes
+    //Only test non-godot classes here
+    //godot::String prompt;
+
+    std::cout << "Start testing: " << std::endl;
+
+    std::unique_ptr<LlamaRunner> lr(new LlamaRunner());
+    std::cout << "Created llama runner" << std::endl;
+
+    std::string prompt = "Tell me about vulkan: ";
+    std::cout << "Prompt: " << prompt << std::endl;
+
+    gpt_params params1 = gpt_params();
+    std::cout << "Model: " << params1.model << std::endl;
+
+    std::string text1 = lr->llama_generate_text(prompt, params1, [](auto a) { return; });
+    std::cout << "Generated text: " << text1 << std::endl;
+
+    gpt_params params2 = gpt_params();
+    params2.model = "../../models/Meta-Llama-3-8B-Instruct.Q5_K_M.gguf";
+    std::cout << "Model: " << params2.model << std::endl;
+    params2.n_predict = 10;
+
+    std::string text2 = lr->llama_generate_text(prompt, params2, [](auto a) { return; });
+    std::cout << "Generated text: " << text2 << std::endl;
+
+    return 0;
+}
