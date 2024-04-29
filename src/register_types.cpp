97f53c27651091d77e04a5984407ffcba034ff3b
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-29 21:02:33 +0800

    refactor: name entry symbol to llm_library_init

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -24,7 +24,7 @@ namespace godot {
 
 	extern "C" {
 		// Initialization.
-		GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+		GDExtensionBool GDE_EXPORT llm_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
 			godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 
 			init_obj.register_initializer(initialize_llm_module);
