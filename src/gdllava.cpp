7b59cff6e99ab6c851e8f98d2e3672423abd21b3
Adriankhl <adrian.k.h.lai@outlook.com>
2024-05-09 00:45:12 +0800

    feat: add GDLlava

---
+++ src/gdllava.cpp
@@ -0,0 +1,11 @@
+#include "gdllava.h"
+
+namespace godot {
+
+void GDLlava::_bind_methods() {
+}
+
+GDLlava::GDLlava() {}
+GDLlava::~GDLlava() {}
+
+} //namespace godot
---
+++ src/gdllava.h
@@ -0,0 +1,44 @@
+#ifndef GDLLAVA_H
+#define GDLLAVA_H
+
+#include "common.h"
+#include "llava_runner.h"
+#include <godot_cpp/classes/mutex.hpp>
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/classes/thread.hpp>
+
+
+namespace godot {
+
+class GDLlava : public Node {
+    GDCLASS(GDLlava, Node)
+
+    private:
+        static void dummy();
+        gpt_params params;
+        std::unique_ptr<LlavaRunner> llava_runner;
+        Ref<Mutex> generate_text_mutex;
+        Ref<Mutex> func_mutex;
+        Ref<Thread> generate_text_thread;
+
+    protected:
+	    static void _bind_methods();
+    
+    public:
+        GDLlava();
+        ~GDLlava();
+        void _exit_tree() override;
+        String get_model_path() const;
+        void set_model_path(const String p_model_path);
+        int32_t get_n_ctx() const;
+        void set_n_ctx(const int32_t p_n_ctx);
+        int32_t get_n_predict() const;
+        void set_n_predict(const int32_t p_n_predict);
+        int32_t get_n_batch() const;
+        void set_n_batch(const int32_t p_n_batch);
+        bool is_running();
+};
+
+} //namespace godot
+
+#endif //GDLLAMA_EMBEDDING_H
