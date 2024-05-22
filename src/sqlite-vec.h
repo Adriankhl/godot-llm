bafa285fb422956c81e8c23f618190a7d36187f3
Adriankhl <adrian.k.h.lai@outlook.com>
2024-05-22 11:23:23 +0800

    build: bump sqlite-vec to v0.0.1-alpha.10

--- src/sqlite-vec.h
+++ src/sqlite-vec.h
@@ -1,19 +1,24 @@
+#ifndef SQLITE_VEC_H
+#define SQLITE_VEC_H
+
+#include "sqlite3ext.h"
+
+#define SQLITE_VEC_VERSION "v0.0.1-alpha.10"
+#define SQLITE_VEC_DATE "2024-05-22T11:15:27Z+0800"
+#define SQLITE_VEC_SOURCE "0a5dcae4c81e0342d3488004a69b3d8e36181031"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "sqlite3ext.h"
-
-#define SQLITE_VEC_VERSION "v0.0.1-alpha.9"
-#define SQLITE_VEC_DATE "2024-05-13T14:12:41Z+0800"
-#define SQLITE_VEC_SOURCE "f532692f7ba476795d9f5ed2f46a9d2269fbab42"
-
-
 int sqlite3_vec_init(sqlite3 *db, char **pzErrMsg,
-                     const sqlite3_api_routines *pApi);
+                  const sqlite3_api_routines *pApi);
 int sqlite3_vec_fs_read_init(sqlite3 *db, char **pzErrMsg,
-                             const sqlite3_api_routines *pApi);
+                          const sqlite3_api_routines *pApi);
+
 
 #ifdef __cplusplus
 }  /* end of the 'extern "C"' block */
 #endif
+
+#endif /* ifndef SQLITE_VEC_H */
